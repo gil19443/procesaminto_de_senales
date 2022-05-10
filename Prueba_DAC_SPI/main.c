ce8e5dbcae7ada9c4f4473d31f0bc7e1888cc383
gil19443 <68124154+gil19443@users.noreply.github.com>
2022-05-10 17:41:47 -0600

    lab

--- Prueba_DAC_SPI/main.c
+++ Prueba_DAC_SPI/main.c
@@ -7,43 +7,6 @@
  * Basado en los ejemplos spi_master.c y timers.c de TivaWare
  * Modificado por Luis Alberto Rivera
  */
-
-
-//*****************************************************************************
-//
-// Copyright (c) 2010-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-//
-//   Redistribution and use in source and binary forms, with or without
-//   modification, are permitted provided that the following conditions
-//   are met:
-//
-//   Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-//
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the
-//   distribution.
-//
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// This is part of revision 2.1.4.178 of the Tiva Firmware Development Package.
-//
 //*****************************************************************************
 
 #include <stdbool.h>
@@ -66,27 +29,6 @@
 #include "utils/uartstdio.h"
 
 //*****************************************************************************
-//
-//! \addtogroup ssi_examples_list
-//! <h1>SPI Master (spi_master)</h1> --- MODIFICADO
-//!
-//! This example shows how to configure the SSI0 as SPI Master.
-//!
-//! This example uses the following peripherals and I/O signals.  You must
-//! review these and change as needed for your own board:
-//! - SSI0 peripheral
-//! - GPIO Port A peripheral (for SSI0 pins)
-//! - SSI0Clk - PA2
-//! - SSI0Fss - PA3
-//! - SSI0Rx  - PA4
-//! - SSI0Tx  - PA5
-//!
-//! This example uses the following interrupt handlers.  To use this example
-//! in your own application you must add these interrupt handlers to your
-//! vector table.
-//! - Timer0IntHandler.
-//
-//*****************************************************************************
 //                              Variables
 //*****************************************************************************
 float y1_n_1 = 0;
@@ -102,15 +44,10 @@ int caso = 0;
 #define VALOR_MAX    4095  // Valor máximo del contador
 #define SPI_FREC  4000000  // Frecuencia para el reloj del SPI
 #define SPI_ANCHO      16  // Número de bits que se envían cada vez, entre 4 y 16
-
-uint16_t cont = 0;
-
 //*****************************************************************************
 // The interrupt handler for the timer interrupt.
 //*****************************************************************************
-void
-Timer0IntHandler(void)
-{
+void Timer0IntHandler(void) {
     uint32_t pui32DataTx[NUM_SPI_DATA]; // la función put pide tipo uint32_t
     uint8_t ui32Index;
 
@@ -150,7 +87,7 @@ Timer0IntHandler(void)
         y2_n_1 = y2;
         x2_n_1 = pui32ADC0Value[0];
         //************************************************************************
-        switch(caso){ //switch para intercambiar entre H1 y H2
+        switch(caso-48){ //switch para intercambiar entre H1 y H2
             case 0: //Formato para que se vean ambas señales en el serial plotter
                 UARTprintf("%d,%d\n", pui32ADC0Value[0],(int)y1);
                 break;
@@ -170,36 +107,85 @@ Timer0IntHandler(void)
     // período del temporizador.
 
 
-        // El DAC espera 16 bits: 4 de configuraciones y 12 del dato
-        pui32DataTx[0] = (uint32_t)((0b0111 << 12) | (0x0FFF & (int)y1));
+    // El DAC espera 16 bits: 4 de configuraciones y 12 del dato
+    pui32DataTx[0] = (uint32_t)((0b0111 << 12) | (0x0FFF & (int)y1));
 
-        // Send data
-        for(ui32Index = 0; ui32Index < NUM_SPI_DATA; ui32Index++)
-        {
-            // Send the data using the "blocking" put function.  This function
-            // will wait until there is room in the send FIFO before returning.
-            // This allows you to assure that all the data you send makes it into
-            // the send FIFO.
-            SSIDataPut(SSI0_BASE, pui32DataTx[ui32Index]);
-        }
+    // Send data
+    for(ui32Index = 0; ui32Index < NUM_SPI_DATA; ui32Index++)
+    {
+        // Send the data using the "blocking" put function.  This function
+        // will wait until there is room in the send FIFO before returning.
+        // This allows you to assure that all the data you send makes it into
+        // the send FIFO.
+        SSIDataPut(SSI0_BASE, pui32DataTx[ui32Index]);
+    }
 
-        // Wait until SSI0 is done transferring all the data in the transmit FIFO.
-        while(SSIBusy(SSI0_BASE))
-        {
-        }
-
-        // Resetear el contador después de VALOR_MAX
-        cont = (cont + 1) % (VALOR_MAX + 1);
+    // Wait until SSI0 is done transferring all the data in the transmit FIFO.
+    while(SSIBusy(SSI0_BASE))
+    {
+    }
 }
 //*****************************************************************************
-//
-// This function sets up UART0 to be used for a console to display information
-// as the example is running.
-//
+// The UART interrupt handler.
 //*****************************************************************************
-void
-InitConsole(void)
+void UARTIntHandler(void)
 {
+    uint32_t ui32Status;
+    // Get the interrrupt status.
+    ui32Status = UARTIntStatus(UART0_BASE, true);
+    // Clear the asserted interrupts
+    UARTIntClear(UART0_BASE, ui32Status);
+    // Loop while there are characters in the receive FIFO.
+    while(UARTCharsAvail(UART0_BASE)){
+        //
+        // Read the next character from the UART and write it back to the UART.
+        //
+        //UARTCharPutNonBlocking(UART0_BASE, UARTCharGetNonBlocking(UART0_BASE));
+        caso = UARTCharGetNonBlocking(UART0_BASE);
+        //
+        // Blink the LED to show a character transfer is occurring.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+
+        //
+        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
+        //
+        SysCtlDelay((uint32_t)(SysCtlClockGet() / (1000 * 3)));
+
+        //
+        // Turn off the LED
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+
+    }
+}
+//*****************************************************************************
+// Send a string to the UART.
+//*****************************************************************************
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+{
+    //
+    // Loop while there are more characters to send.
+    //
+    while(ui32Count--)
+    {
+        //
+        // Write the next character to the UART.
+        //
+        UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
+    }
+}
+//*****************************************************************************
+// configuracion para usar el UART0
+//*****************************************************************************
+void InitConsole(void) {
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    FPUEnable();
+    FPULazyStackingEnable();
     // Enable GPIO port A which is used for UART0 pins.
     // TODO: change this to whichever GPIO port you are using.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -222,55 +208,39 @@ InitConsole(void)
 
     // Initialize the UART for console I/O.
     UARTStdioConfig(0, 230400, 16000000);
+
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+                                (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                                 UART_CONFIG_PAR_NONE));
+
+    //
+    // Enable the UART interrupt.
+    //
+    IntEnable(INT_UART0);   // INT_UART0 se vuelve INT_UART0_TM4C123
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+
+    //
+    // Prompt for text to be entered.
+    //
+    UARTSend((uint8_t *)"\nEnter text: ", 14);
+
 }
-
 //*****************************************************************************
-// Configure SSI0 in master Freescale (SPI) mode.
-// Se configura el timer y su interrupción. El SPI se configura para enviar 16
-// bits. El envío se hace en la rutina de interrupción del timer.
+// configuracion para usar el ADC0
 //*****************************************************************************
-int
-main(void)
-{
-    uint32_t pui32residual[NUM_SPI_DATA];
-    uint16_t freq_timer = 1000;    // En Hz
-
-// ------ Configuración del reloj ---------------------------------------------
-    // Set the clock to run at 80 MHz (200 MHz / 2.5) using the PLL.
-    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
-                   SYSCTL_XTAL_16MHZ); // 80 MHz. SÍ FUNCIONA
-// ----------------------------------------------------------------------------
-
-    // Set up the serial console to use for displaying messages.  This is
-        // just for this example program and is not needed for ADC operation.
-        InitConsole();
+void InitADC(void) {
     // The ADC0 peripheral must be enabled for use.
-        SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    // The SSI0 peripheral must be enabled for use.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 
-    // For this example SSI0 is used with PortA[5:2].  The actual port and pins
-    // used may be different on your part, consult the data sheet for more
-    // information.  GPIO port A needs to be enabled so these pins can be used.
-    // TODO: change this to whichever GPIO port you are using.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);//habilita el puerto F
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2|GPIO_PIN_3);//Pines de salida para los leds
-
-
-    // Configure the pin muxing for SSI0 functions on port A2, A3, A4, and A5.
-    // This step is not necessary if your part does not support pin muxing.
-    // TODO: change this to select the port/pin you are using.
-    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
-    GPIOPinConfigure(GPIO_PA3_SSI0FSS);
-    GPIOPinConfigure(GPIO_PA4_SSI0RX);
-    GPIOPinConfigure(GPIO_PA5_SSI0TX);
 
     // Select the analog ADC function for these pins.
     // Consult the data sheet to see which functions are allocated per pin.
     // TODO: change this to select the port/pin you are using.
     GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3);
+
 
     // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
     // will do a single sample when the processor sends a signal to start the
@@ -295,6 +265,82 @@ main(void)
     // Clear the interrupt status flag.  This is done to make sure the
     // interrupt flag is cleared before we sample.
     ADCIntClear(ADC0_BASE, 3);
+    // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
+    // will do a single sample when the processor sends a signal to start the
+    // conversion.  Each ADC module has 4 programmable sequences, sequence 0
+    // to sequence 3.  This example is arbitrarily using sequence 3.
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+
+    // Configure step 0 on sequence 3.  Sample channel 0 (ADC_CTL_CH0) in
+    // single-ended mode (default) and configure the interrupt flag
+    // (ADC_CTL_IE) to be set when the sample is done.  Tell the ADC logic
+    // that this is the last conversion on sequence 3 (ADC_CTL_END). Sequence
+    // 3 has only one programmable step.  Sequence 1 and 2 have 4 steps, and
+    // sequence 0 has 8 programmable steps.  Since we are only doing a single
+    // conversion using sequence 3 we will only configure step 0.  For more
+    // information on the ADC sequences and steps, reference the datasheet.
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+                             ADC_CTL_END);
+
+    // Since sample sequence 3 is now configured, it must be enabled.
+    ADCSequenceEnable(ADC0_BASE, 3);
+
+    // Clear the interrupt status flag.  This is done to make sure the
+    // interrupt flag is cleared before we sample.
+    ADCIntClear(ADC0_BASE, 3);
+}
+//*****************************************************************************
+// configuracion para usar el Timer0
+//*****************************************************************************
+void InitTimer (void){
+
+    uint16_t freq_timer = 1000;    // En Hz
+    // Enable the peripherals used by this example.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+
+
+    // Configure the two 32-bit periodic timers.
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+
+    // El tercer argumento determina la frecuencia. El reloj se puede obtener
+    // con SysCtlClockGet().
+    // La frecuencia está dada por SysCtlClockGet()/(valor del 3er argumento).
+    // Ejemplos: Si se pone SysCtlClockGet(), la frecuencia será de 1 Hz.
+    //           Si se pone SysCtlClockGet()/1000, la frecuencia será de 1 kHz
+    TimerLoadSet(TIMER0_BASE, TIMER_A, (uint32_t)(SysCtlClockGet()/freq_timer));
+
+    // Setup the interrupt for the timer timeout.
+    IntEnable(INT_TIMER0A);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Enable the timers.
+    TimerEnable(TIMER0_BASE, TIMER_A);
+}
+//*****************************************************************************
+// configuracion para usar el SPI
+//*****************************************************************************
+void InitSPI (void){
+
+    uint32_t pui32residual[NUM_SPI_DATA];
+    // The SSI0 peripheral must be enabled for use.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+
+    // For this example SSI0 is used with PortA[5:2].  The actual port and pins
+    // used may be different on your part, consult the data sheet for more
+    // information.  GPIO port A needs to be enabled so these pins can be used.
+    // TODO: change this to whichever GPIO port you are using.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+
+    // Configure the pin muxing for SSI0 functions on port A2, A3, A4, and A5.
+    // This step is not necessary if your part does not support pin muxing.
+    // TODO: change this to select the port/pin you are using.
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
+    GPIOPinConfigure(GPIO_PA3_SSI0FSS);
+    GPIOPinConfigure(GPIO_PA4_SSI0RX);
+    GPIOPinConfigure(GPIO_PA5_SSI0TX);
+
 
     // Configure the GPIO settings for the SSI pins.  This function also gives
     // control of these pins to the SSI hardware.  Consult the data sheet to
@@ -331,29 +377,33 @@ main(void)
     while(SSIDataGetNonBlocking(SSI0_BASE, &pui32residual[0]))
     {
     }
+}
+//*****************************************************************************
+// Configure SSI0 in master Freescale (SPI) mode.
+// Se configura el timer y su interrupción. El SPI se configura para enviar 16
+// bits. El envío se hace en la rutina de interrupción del timer.
+//*****************************************************************************
+int
+main(void)
+{
+//***********************Configuración del reloj*******************************
+    // Set the clock to run at 80 MHz (200 MHz / 2.5) using the PLL.
+    SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ); // 80 MHz. SÍ FUNCIONA
+//*****************************************************************************
 
-    // Enable the peripherals used by this example.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    InitConsole(); //configuracion UART
+    InitADC(); //configuracion ADC
+    InitTimer(); //configuracion Timer
+    InitSPI(); //configuracion del SPI
 
+//******************Configuraciones generales**********************************
     // Enable processor interrupts.
     IntMasterEnable();
 
-    // Configure the two 32-bit periodic timers.
-    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
-
-    // El tercer argumento determina la frecuencia. El reloj se puede obtener
-    // con SysCtlClockGet().
-    // La frecuencia está dada por SysCtlClockGet()/(valor del 3er argumento).
-    // Ejemplos: Si se pone SysCtlClockGet(), la frecuencia será de 1 Hz.
-    //           Si se pone SysCtlClockGet()/1000, la frecuencia será de 1 kHz
-    TimerLoadSet(TIMER0_BASE, TIMER_A, (uint32_t)(SysCtlClockGet()/freq_timer));
-
-    // Setup the interrupt for the timer timeout.
-    IntEnable(INT_TIMER0A);
-    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-
-    // Enable the timers.
-    TimerEnable(TIMER0_BASE, TIMER_A);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);//habilita el puerto F
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2|GPIO_PIN_3);//Pines de salida para los leds
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
 
     // El envío se hace al darse la interrupción del timer, para que sea en
     // intervalos regulares. Luego de las configuraciones, el programa se
