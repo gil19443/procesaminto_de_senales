b04082bdab233d289d29476cd95a8c1795f104df
gil19443 <68124154+gil19443@users.noreply.github.com>
2022-05-16 12:04:32 -0600

    Update main.c

--- Proyecto_signals/main.c
+++ Proyecto_signals/main.c
@@ -152,9 +152,7 @@ void UARTIntHandler(void){
         */
 
         data = UARTgets(filtro,21);
-        UARTprintf("%d\n",data);
-        UARTprintf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",filtro[0],filtro[1],filtro[2],filtro[3],filtro[4],filtro[5],filtro[6],filtro[7],filtro[8],filtro[9],filtro[10],filtro[11],filtro[12],filtro[13],filtro[14],filtro[15],filtro[16],filtro[17],filtro[18],filtro[19]);
-       /*
+        /*
         for(n=0;n<20;n++){
             UARTCharPutNonBlocking(UART0_BASE, filtro[n]);
         }
@@ -175,6 +173,9 @@ void UARTIntHandler(void){
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
 
     }
+    UARTprintf("%d\n",data);
+    UARTprintf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",filtro[0],filtro[1],filtro[2],filtro[3],filtro[4],filtro[5],filtro[6],filtro[7],filtro[8],filtro[9],filtro[10],filtro[11],filtro[12],filtro[13],filtro[14],filtro[15],filtro[16],filtro[17],filtro[18],filtro[19]);
+
 }
 //*****************************************************************************
 // This function sets up UART0
